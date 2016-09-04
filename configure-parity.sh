f858f8f86b69cb3ecaab3b63a437f0eb9a2241d4
Igor Barinov <igorbarinov@me.com>
2016-09-04 14:58:17 -0700

    removed sudo from bash scripts, added option to force seal blocks

--- configure-parity.sh
+++ configure-parity.sh
@@ -74,6 +74,6 @@ EOL
 
 DAPP_PASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)
  
-command="parity: parity -lsync=trace,txqueue=trace,own_tx=trace --reseal-on-txs all --dapps-hosts  all --dapps-interface 0.0.0.0 --dapps-port 8002 --dapps-user user --dapps-pass $DAPP_PASSWORD --chain $HOME/chain.json --author ${address} --unlock ${address} --password $HOME/.parity-pass --rpccorsdomain \"*\" --jsonrpc-hosts=all --jsonrpc-interface all >&1 1>>/var/log/parity.log 2>&1"
+command="parity: parity -lsync=trace,txqueue=trace,own_tx=trace --reseal-on-txs all --force-sealing --dapps-hosts  all --dapps-interface 0.0.0.0 --dapps-port 8002 --dapps-user user --dapps-pass $DAPP_PASSWORD --chain $HOME/chain.json --author ${address} --unlock ${address} --password $HOME/.parity-pass --rpccorsdomain \"*\" --jsonrpc-hosts=all --jsonrpc-interface all >&1 1>>/var/log/parity.log 2>&1"
 echo $command >> /etc/goreman/Procfile 
 #  --force-sealing
