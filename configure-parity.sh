e63f263be0f7afd6b980b623f779c8b7c3c5e14a
Igor Barinov <igorbarinov@me.com>
2016-09-06 23:42:10 -0700

    fix

--- configure-parity.sh
+++ configure-parity.sh
@@ -11,17 +11,7 @@ echo "user: $(whoami)"
 PASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)
 echo $PASSWORD > $HOME/.parity-pass
 
-expect_out= expect -c "
-spawn sudo parity account new
-puts $HOME
-expect \"Type password: \"
-send ${PASSWORD}\n
-expect \"Repeat password: \"
-send ${PASSWORD}\n
-interact
-"
-
-echo $expect_out
+parity account new --password $HOME/.parity-pass
 
 address=0x$(parity account list | awk 'END{print}' | tr -cd '[:alnum:]._-')
 
